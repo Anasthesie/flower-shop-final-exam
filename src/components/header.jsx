fbf5b595f8168864c45e407c4de410d15c3916cf
Anasthesie <118045856+Anasthesie@users.noreply.github.com>
2025-08-30 21:56:04 +0400

    replaced a tag with link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router";
+
 export default function Header() {
   return (
     <header className="flex justify-between items-center  px-6 py-4  bg-green text-white">
       <nav>
         <ul className="flex  gap-8">
           <li>
-            <a href="/shop">Shop</a>
+            <Link to="/shop">Shop</Link>
           </li>
           <li>
-            <a href="/guide">Guide</a>
+            <Link to="/guide">Guide</Link>
           </li>
           <li>
-            <a href="/products">Products</a>
+            <Link to="/products">Products</Link>
           </li>
           <li>
-            <a href="/fertilizer">Fertilizer</a>
+            <Link to="/fertilizer">Fertilizer</Link>
           </li>
         </ul>
       </nav>
